762a3400f1572b10b7e40cb05d2309a10ca401e6
canam889 <canam889@users.noreply.github.com>
2022-09-21 12:24:38 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD8Yyuiuyudai6U0__trtryrnfvgHlwgf_Y',
+  authDomain: 'techhub-ui-2df54fww3b4.firebaseapp.com',
+  databaseURL: 'https://techhub-ui-2df54fww3b4.firebaseio.com',
+  projectId: 'techhub-ui-2df54fww3b4',
+  storageBucket: 'techhub-ui-2df54fww3b4.appspot.com',
+  messagingSenderId: '8898748979878750',
+  appId: '1:871456338750:web:57121ce924e7ad6666ssaef',
+  measurementId: 'G-8TBewrwetgfdcKX5'
+}
